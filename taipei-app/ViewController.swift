56652406951ab55fc209ef7752dbbff23a436f5a
Simon Lee <dev.simonlee@gmail.com>
2017-05-01 10:55:24 +0800

    Added sample annotation and cafe request

--- taipei-app/ViewController.swift
+++ taipei-app/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import Mapbox
+import PromiseKit
+import Alamofire
 
 class ViewController: UIViewController {
+    @IBOutlet var mapView: MGLMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 25.0376486, longitude: 121.5617326)
+        point.title = "台北市政府"
+        point.subtitle = "台北市信義區市府路1號"
+        
+        mapView.addAnnotation(point)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func cafesRequest() -> Request {
+        let baseURL = "104.236.125.139:3000"
+        let endpoint = "\(baseURL)/cafes"
+        let request = Alamofire.request(endpoint, method: .get)
+        return request
     }
-
-
 }
 
+extension ViewController: MGLMapViewDelegate {
+    
+}
