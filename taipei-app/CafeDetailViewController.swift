c47667a9f49eb900afce546513089b72ab4e58d5
Simon Lee <dev.simonlee@gmail.com>
2017-06-28 11:11:54 +0800

    Added cell for cafe rating

--- taipei-app/CafeDetailViewController.swift
+++ taipei-app/CafeDetailViewController.swift
@@ -11,21 +11,25 @@ import Mapbox
 
 class CafeDetailViewController: UIViewController {
     var annotation: CafeAnnotation?
-
-    @IBOutlet weak var nameLabel: UILabel!  {
-        didSet {
-            nameLabel.text = annotation?.title!
-        }
-    }
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension CafeDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let ratingCell = tableView.dequeueReusableCell(withIdentifier: "ratingCell", for: indexPath) as! RatingTableViewCell
+        
+        ratingCell.nameLabel.text = annotation?.title
+        
+        return ratingCell
     }
 }
---
+++ taipei-app/RatingTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RatingTableViewCell.swift
+//  taipei-app
+//
+//  Created by Shao-Ping Lee on 6/28/17.
+//  Copyright © 2017 Simon Lee. All rights reserved.
+//
+
+import UIKit
+
+class RatingTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
